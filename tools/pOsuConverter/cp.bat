9ea055767e8ab5bf8ab29fad1e0e62d92116b496
ICEJJ <lpc0503@gmail.com>
2018-12-18 13:42:13 +0800

    [develop] add new song

--- tools/pOsuConverter/cp.bat
+++ tools/pOsuConverter/cp.bat
@@ -4,11 +4,10 @@ cls
 
 echo COMPILING...
 
-g++ %1.cpp -o %1.exe
+g++ main.cpp poct.cpp -o %1.exe
 
 echo FINISH
 
 if %ERRORLEVEL% EQU 0 (
-
-	%1 -i test2.txt -o t2.txt
+	%1 -i %2.txt -o %3.txt
 )
